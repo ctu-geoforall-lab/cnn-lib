ctu-geoforall-lab/cnn-lib

commit d65f451bc0cb594f0c2b5650a26538c0ec31f7f9

detect: avoid printing model summary (#25)
some bug in new versions of TF destroys it when used

cnn_lib/detect.py
@@ -41,7 +41,7 @@ def run(data_dir, model, in_weights_path, visualization_path, batch_size,
             tf.keras.utils.set_random_seed(seed)
 
     model = create_model(model, len(id2code), nr_bands, tensor_shape,
-                         backbone=backbone)
+                         backbone=backbone, verbose=0)
 
     # val generator used for both the training and the detection
     val_generator = AugmentGenerator(

commit 1db886248d932f81ecd8565faa406e604068cd4c

detect: make verbose again
commit d65f451 broke the detection to produce zeros everywhere

TODO: Inspect when having GPU again - maybe it behaves different for GPUs and CPUs

cnn_lib/detect.py
@@ -41,7 +41,7 @@ def run(data_dir, model, in_weights_path, visualization_path, batch_size,
             tf.keras.utils.set_random_seed(seed)
 
     model = create_model(model, len(id2code), nr_bands, tensor_shape,
-                         backbone=backbone, verbose=0)
+                         backbone=backbone, verbose=1)
 
     # val generator used for both the training and the detection
     val_generator = AugmentGenerator(